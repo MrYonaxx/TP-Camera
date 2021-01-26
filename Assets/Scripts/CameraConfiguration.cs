6a76f149afb61195b4926ebc01dd92e218c7d444
Clarence <diyobe2207@gmail.com>
2021-01-26 11:07:54 +0100

    Implemented CameraConfiguration & CameraController

---
+++ Assets/Scripts/CameraConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CameraConfiguration
+{
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+    public float roll = 0.0f;
+    public Vector3 pivot = new Vector3(0.0f, 1.0f, -10.0f);
+    public float distance = 0.0f;
+    public float fieldOfView = 60.0f;
+
+    public Quaternion GetRotation()
+    {
+        return Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    public Vector3 GetPosition()
+    {
+        Vector3 offset = GetRotation() * (Vector3.back * distance);
+        return pivot + offset;
+    }
+
+    public void DrawGizmos(Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(pivot, 0.25f);
+        Vector3 position = GetPosition();
+        Gizmos.DrawLine(pivot, position);
+        Gizmos.matrix = Matrix4x4.TRS(position, GetRotation(), Vector3.one);
+        Gizmos.DrawFrustum(Vector3.zero, fieldOfView, 0.5f, 0f, Camera.main.aspect);
+        Gizmos.matrix = Matrix4x4.identity;
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] CameraConfiguration cameraConfiguration;
+    [SerializeField] Camera camera;
+
+    private static CameraController _instance;
+
+    void Awake()
+    {
+
+        if (_instance == null)
+        {
+
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+            //Rest of your Awake code
+            //Supprimer les commentaires quand fini de toucher au Awake
+
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        camera.transform.position = cameraConfiguration.GetPosition();
+        camera.transform.rotation = cameraConfiguration.GetRotation();
+
+        camera.fieldOfView = cameraConfiguration.fieldOfView;
+    }
+
+    private void OnDrawGizmos()
+    {
+        cameraConfiguration.DrawGizmos(Color.red);
+    }
+}
