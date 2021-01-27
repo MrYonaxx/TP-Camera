6d8ceb614f622f40027dca33fe2f5728ae6fa4e2
Clarence <diyobe2207@gmail.com>
2021-01-27 15:33:18 +0100

    Question 5

---
+++ Assets/DollyView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollyView : AView
+{
+	public float roll;
+	public float distance;
+	public float fov;
+
+	public Transform target;
+	public Rail rail;
+	public Vector3 railPosition;
+
+	public float speed;
+
+	public override CameraConfiguration GetConfiguration()
+	{
+		CameraConfiguration res = new CameraConfiguration();
+		res.roll = roll;
+		res.fieldOfView = fov;
+		distance += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		railPosition = rail.GetPosition(distance);
+		res.pivot = railPosition;
+
+		Vector3 dir = (target.position - res.pivot).normalized;
+		res.yaw = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+		res.pitch = -Mathf.Asin(dir.y) * Mathf.Rad2Deg;
+
+		return res;
+	}
+}
---
+++ Assets/Rail.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+    [SerializeField] Transform[] children;
+    public bool isLoop = false;
+    float length = 0.0f;
+
+    private void Awake()
+    {
+        children = GetComponentsInChildren<Transform>();
+    }
+
+    private void Start()
+    {
+        for (int i = 1; i < children.Length - 1; i++)
+        {
+            length += Vector3.Distance(children[i].position, children[i + 1].position);
+        }
+        if (isLoop)
+        {
+            length += Vector3.Distance(children[children.Length - 1].position, children[1].position);
+        }
+
+    }
+
+    public void DrawGizmos()
+    {
+        if (children.Length == 0)
+            return;
+
+        for (int i = 1; i < children.Length; i++)
+        {
+            if (i + 1 < children.Length)
+                Gizmos.DrawLine(children[i].position, children[i + 1].position);
+        }
+
+        if (isLoop)
+        {
+            Gizmos.DrawLine(children[children.Length - 1].position, children[1].position);
+        }
+    }
+
+    public float GetLength()
+    {
+        return length;
+    }
+
+    public Vector3 GetPosition(float distance)
+    {
+        float exempleLength = 0.0f;
+        if (isLoop)
+        {
+            while (distance < 0)
+            {
+                distance += length;
+            }
+            distance = distance % length;
+
+        }
+
+        for (int i = 1; i < children.Length - 1; i++)
+        {
+            exempleLength = Vector3.Distance(children[i].position, children[i + 1].position);
+            if(distance < exempleLength)
+            {
+                return Vector3.Lerp(children[i].position, children[i + 1].position, distance/exempleLength);
+            }
+            else
+            {
+                distance -= exempleLength;
+            }
+        }
+        if (isLoop)
+        {
+            exempleLength = Vector3.Distance(children[children.Length - 1].position, children[1].position);
+            if (distance < exempleLength)
+            {
+                return Vector3.Lerp(children[children.Length - 1].position, children[1].position, distance / exempleLength);
+            }
+            else
+            {
+                distance -= exempleLength;
+            }
+        }
+        return children[children.Length - 1].position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawGizmos();
+    }
+}
